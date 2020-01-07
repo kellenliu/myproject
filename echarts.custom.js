53ecb91aa3e81e5a75d965c414165815b3da8e72
kellen <15196371692@139.com>
2020-01-07 14:01:27 +0800

    updaten

---
+++ echarts.custom.js
@@ -0,0 +1,6 @@
+// 引入 echarts 主模块。
+export * from 'echarts/src/echarts';
+// 引入饼图。
+import 'echarts/src/chart/pie';
+// 在这个场景下，可以引用 `echarts/src` 或者 `echarts/lib` 下的文件（但是不可混用），
+// 参见下方的解释：“引用 `echarts/lib/**` 还是 `echarts/src/**`”。
